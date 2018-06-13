00663365bc6cbe68f92c348979ea4868a7bdc236
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-06-13 08:03:02 -0300

    Integrate Prettier with ESLint

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,40 +1,13 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
-class Template extends React.Component {
-  render() {
-    const { location, children } = this.props
-    let header
-    if (location.pathname === '/') {
-      header = (
-        <h1>
-          <Link to={'/'} >
-            Gatsby Starter Blog
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3>
-          <Link to={'/'}>
-            Gatsby Starter Blog
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <div>
-        {header}
-        {children()}
-      </div>
-    )
-  }
+const Template = ({ location, children }) => {
+  return children()
 }
 
 Template.propTypes = {
   children: React.PropTypes.func,
   location: React.PropTypes.object,
-  route: React.PropTypes.object,
+  route: React.PropTypes.object
 }
 
 export default Template
