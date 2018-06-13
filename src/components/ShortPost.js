ae955e2d6af69c90f414aa3e149f61516906b54a
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-06-13 17:38:05 -0300

    Add pages index

---
+++ src/components/ShortPost.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+import colors from '../aesthetics/colors'
+
+const Article = styled.article`
+  position: relative;
+  display: flex;
+
+  &:not(:first-child) {
+    padding: 100px;
+  }
+`
+
+const Href = styled(Link)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`
+
+const Left = styled.div`
+  margin-right: 20px;
+`
+const Right = styled.div``
+
+const Title = styled.h3`
+  color: ${colors.orange[5]};
+  margin-bottom: 0;
+`
+
+const PostDate = styled.time`
+  color: ${colors.gray[1]};
+  font-size: 12px;
+`
+
+const Description = styled.section`
+  margin-top: 10px;
+  color: ${colors.gray[2]};
+`
+
+const ShortPost = ({ url, title, description, createdAt }) => {
+  return (
+    <Article>
+      <Href to={url} />
+
+      <Left>
+        <PostDate>{createdAt}</PostDate>
+      </Left>
+      <Right>
+        <Title>{title}</Title>
+
+        <Description>
+          <span>{description}</span>
+        </Description>
+      </Right>
+    </Article>
+  )
+}
+
+ShortPost.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  createdAt: PropTypes.string.isRequired
+}
+
+export default ShortPost
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,22 @@ import React from 'react'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
+
+import Container from '../components/layout/Container'
+import ShortPost from '../components/ShortPost'
+
+const Content = styled.div`
+  padding: 100px 0;
+`
+
+const Posts = styled.div``
+
+const Post = styled(Container)`
+  &:not(:first-child) {
+    padding-top: 40px;
+  }
+`
 
 class BlogIndex extends React.Component {
   render () {
@@ -9,29 +25,46 @@ class BlogIndex extends React.Component {
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
     return (
-      <div>
+      <Content>
         <Helmet title={siteTitle} />
 
-        {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3>
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
-      </div>
+        <Posts>
+          {posts.map(({ node }) => {
+            const title = get(node, 'frontmatter.title') || node.fields.slug
+
+            return (
+              <Post key={node.fields.slug}>
+                <ShortPost
+                  key={node.fields.slug}
+                  url={node.fields.slug}
+                  title={title}
+                  description={node.frontmatter.description}
+                  createdAt={node.frontmatter.date}
+                />
+              </Post>
+            )
+          })}
+        </Posts>
+      </Content>
     )
   }
 }
 
 export default BlogIndex
+
+// return (
+//   <div key={node.fields.slug}>
+//     <Container>
+//       <h3>
+//         <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+//           {title}
+//         </Link>
+//       </h3>
+//       <small>{node.frontmatter.date}</small>
+//       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+//     </Container>
+//   </div>
+// )
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -50,6 +83,7 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "DD MMMM, YYYY")
             title
+            description
           }
         }
       }
