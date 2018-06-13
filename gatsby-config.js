1dd63c5d13a1d2063775b9af3b7db7665ca201b7
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-06-13 07:44:36 -0300

    Update metadata to correspond to Strive/Vincae

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Blog",
-    author: "Kyle Mathews",
+    title: "Strive Blog",
+    author: "Vincae Entertainment",
   },
   plugins: [
     {
