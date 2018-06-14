31101ff5e883a1524a268ab1fa7eb82436279d5d
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-06-13 21:50:30 -0300

    Implement Google Analytics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import GoogleAnalytics from 'react-ga'
 import 'normalize.css'
 
 import Header from '../components/layout/Header/index'
 import Footer from '../components/layout/Footer'
+import isDefined from '../utils/isDefined'
 
 import typography from '../aesthetics/typography'
 import './globals.css'
 
 typography.injectStyles()
+
+const GoogleAnalyticsPageviewParam =
+  isDefined(window) && window.location.pathname + window.location.search
+
+GoogleAnalytics.initialize('UA-120846585-1')
+GoogleAnalytics.pageview(GoogleAnalyticsPageviewParam)
 
 const Template = ({ children }) => {
   return (
---
+++ src/utils/isDefined.js
@@ -0,0 +1,1 @@
+export default prop => typeof prop !== 'undefined'
